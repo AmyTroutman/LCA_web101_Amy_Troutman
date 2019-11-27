481b8381bba4b4ff1291649b9665dabc40cf6391
AmyTroutman <amyrtroutman@gmail.com>
2019-11-26 20:29:18 -0600

    started some js. did some styling.

---
+++ myPortfolio/src/js/ticTacToe.js
@@ -0,0 +1,10 @@
+var gameMarker = "x";
+function changeMarkerToX() {
+  var gameMarker = "X";
+  console.log("The x button was clicked!");
+}
+function changeMarkerToO() {
+  var gameMarker = "O";
+  console.log("The o button was clicked!");
+}
+function placeMark() {}
