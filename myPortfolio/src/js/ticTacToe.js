91008c911a6a89309b05f6c15e0d78e627341135
AmyTroutman <amyrtroutman@gmail.com>
2019-12-03 13:48:51 -0600

    sloooooowly making progress. got an id grabber.

--- myPortfolio/src/js/ticTacToe.js
+++ myPortfolio/src/js/ticTacToe.js
@@ -11,19 +11,18 @@ function changeMarkerToO() {
   console.log("The o button was clicked!");
 }
 
-function placeMarker() {
+//get id of clicked box. does this need to be a separate function? if not, how do I "pass" the info?
+function getName(clicked) {
+  console.log(getName.caller.arguments[0].target.id);
+}
+
+function placeMarker(getName, gameMarker) {
   //do I need this?? from codepen js tictactoe, but I maybe don't need it. Is checking if box is empty.
   //What I need is for the marker to be placed!
-
-  if (cells.contentText == "") {
-    this.contentText = gameMarker;
+  if (getName.contentText == "") {
+    //I don't think I want this.
+    this.innerHTML(gameMarker);
   }
 
   console.log("The marker was placed!");
-}
-
-//get id of clicked box. does this need to be a separate function? if not, how do I "pass" the info?
-function getName(clicked) {
-  var name = clicked;
-  console.log("id got");
 }
