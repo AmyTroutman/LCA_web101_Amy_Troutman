5a032e9420fc6da653448c4f26e079da077c09bb
AmyTroutman <amyrtroutman@gmail.com>
2019-12-09 12:17:13 -0600

    easy peasy

---
+++ myPortfolio/src/js/hanoi.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  let $block = null;
+
+  $("[data-column]").click(function() {
+    if ($block) {
+      $(this).append($block);
+      $block = null;
+    } else {
+      $block = $(this)
+        .children()
+        .last()
+        .detach();
+    }
+  });
+});
