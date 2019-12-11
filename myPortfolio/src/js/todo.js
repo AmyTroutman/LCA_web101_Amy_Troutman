9eaf2bc0a11110320e9638cd56b465844756373c
AmyTroutman <amyrtroutman@gmail.com>
2019-12-10 19:42:37 -0600

    added geo locatey bit

--- myPortfolio/src/js/todo.js
+++ myPortfolio/src/js/todo.js
@@ -119,3 +119,18 @@ $(document).ready(function($) {
     }
   });
 });
+
+/* Geolocation stuff */
+$.ajax({
+  url: "https://geolocation-db.com/jsonp",
+  jsonpCallback: "callback",
+  dataType: "jsonp",
+  success: function(location) {
+    $("#country").html(location.country_name);
+    $("#state").html(location.state);
+    $("#city").html(location.city);
+    $("#latitude").html(location.latitude);
+    $("#longitude").html(location.longitude);
+    $("#ip").html(location.IPv4);
+  }
+});
